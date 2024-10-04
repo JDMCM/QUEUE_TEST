af6ea8abf93ba8faa08041b53e24f7442a865a2f
JDMCM <jmcmahon@trinity.edu>
2024-10-04 14:55:01 -0500

    It loops now

--- src/main.rs
+++ src/main.rs
@@ -70,30 +70,32 @@ fn main() {
     let mut heap: BinaryHeap<Keyval> = BinaryHeap::new(); //intialize the queue
 
     // j and i got flipped from standard notation i nested in j instead of the the other way around
-    for j in 0..data.len() {
-        let mut exc = Vec::new(); //each bif loop has a new exculsion list as the heap is emptied each j iteration
-        exc.push(data[j][0].id);
-        heap.push(data[j][0]);
-        for i in 0..data[j].len() {
-            // if the id of the item to be added is not already in the exculsion list 
-            // it is added to the heap then it's id is added to the exclusion list
-            if  !exc.contains(&data[j][i].id)  { 
+    for _i in 0..10 {
+        for j in 0..data.len() {
+            let mut exc = Vec::new(); //each bif loop has a new exculsion list as the heap is emptied each j iteration
+            exc.push(data[j][0].id);
+            heap.push(data[j][0]);
+            for i in 0..data[j].len() {
+                // if the id of the item to be added is not already in the exculsion list 
+                // it is added to the heap then it's id is added to the exclusion list
+                if  !exc.contains(&data[j][i].id)  { 
 
-                exc.push(data[j][i].id);
-                heap.push(data[j][i]);
-            } else if !heap.is_empty()  {
-                while  !heap.is_empty() && heap.peek().unwrap().id != data[j][i].id {
-                    let y = heap.pop().unwrap().id;
-                    exc.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
+                    exc.push(data[j][i].id);
+                    heap.push(data[j][i]);
+                } else if !heap.is_empty()  {
+                    while  !heap.is_empty() && heap.peek().unwrap().id != data[j][i].id {
+                        let y = heap.pop().unwrap().id;
+                        exc.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
+                    }
                 }
+                
             }
-            
-        }
-        //clears the queue after everything in a big timestep has been processed 
-        while !heap.is_empty() { 
-            heap.pop();
-            //exc.pop();
-            
+            //clears the queue after everything in a big timestep has been processed 
+            while !heap.is_empty() { 
+                heap.pop();
+                //exc.pop();
+                
+            }
         }
     }
 
@@ -109,30 +111,32 @@ fn main() {
     let mut heap1: bucketqueue::Bqueue<Keyval> = bucketqueue::Bqueue::new(((max/delta).ceil()+1.0) as usize,delta); //intialize the Bucket queue
 
     // j and i got flipped 
-    for j in 0..data.len() {
-        let mut exc1 = Vec::new(); //each j loop has a new exculsion list as the heap is emptied each j iteration
-        exc1.push(data[j][0].id);
-        heap1.push(data[j][0],data[j][0].val.time);
-        for i in 0..data[j].len() {
-            // if the id of the item to be added is not already in the exculsion list 
-            // it is added to the heap then it's id is added to the exclusion list
-            if  !exc1.contains(&data[j][i].id)  { 
+    for _i in 0..10 {
+        for j in 0..data.len() {
+            let mut exc1 = Vec::new(); //each j loop has a new exculsion list as the heap is emptied each j iteration
+            exc1.push(data[j][0].id);
+            heap1.push(data[j][0],data[j][0].val.time);
+            for i in 0..data[j].len() {
+                // if the id of the item to be added is not already in the exculsion list 
+                // it is added to the heap then it's id is added to the exclusion list
+                if  !exc1.contains(&data[j][i].id)  { 
 
-                exc1.push(data[j][i].id);
-                heap1.push(data[j][i], data[j][i].val.time);
-            } else if !heap1.is_empty()  {
-                while  !heap1.is_empty() && heap1.peek().unwrap().id != data[j][i].id {
-                    let y = heap1.pop().unwrap().id;
-                    exc1.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
+                    exc1.push(data[j][i].id);
+                    heap1.push(data[j][i], data[j][i].val.time);
+                } else if !heap1.is_empty()  {
+                    while  !heap1.is_empty() && heap1.peek().unwrap().id != data[j][i].id {
+                        let y = heap1.pop().unwrap().id;
+                        exc1.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
+                    }
                 }
+                
             }
-            
-        }
-        //clears the queue after everything in a big timestep has been processed 
-        while !heap1.is_empty() { 
-            heap1.pop();
-            //exc.pop();
-            
+            //clears the queue after everything in a big timestep has been processed 
+            while !heap1.is_empty() { 
+                heap1.pop();
+                //exc.pop();
+                
+            }
         }
     }
 
