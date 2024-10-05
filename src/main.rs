e8579d15d2804f193b0de4535f335887c5540fa5
JDMCM <jmcmahon@trinity.edu>
2024-10-04 20:56:31 -0500

    It has been redone my liege

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,8 @@ fn main() {
     pub struct Keyval {
         pub key:OrderedFloat<f64>, //the time the pair collides at
         pub val:csvreader::Rec, //all information p1,p2,p1x,p2x .. etc
-        pub id:(f64,f64)        //p1,p2
+        pub id:(f64,f64),        //p1,p2
+        pub index: usize        //so it can be looked up easy within the data matrix
     }
     // keyval needs to be ordered so I can stick it in a priority queue
     impl Ord for Keyval {
@@ -61,40 +62,38 @@ fn main() {
         let poppy = arecord.pop_front().unwrap();
         //uses the max to find which global timestep each record belongs to then places them 
         //in the corresponding vector
-        let index:usize = (poppy.time/(max/500.0)).floor() as usize; 
-        data[index].push(Keyval{key:OrderedFloat(poppy.time),val:poppy,id:(poppy.p1,poppy.p2)});
+        let index:usize = (poppy.time/(max/500.0)).floor() as usize;
+        let ind = data[index].len();
+        data[index].push(Keyval{key:OrderedFloat(poppy.time),val:poppy,id:(poppy.p1,poppy.p2),index: ind});
 
     }
     let now = Instant::now();
 
-    let mut heap: BinaryHeap<Keyval> = BinaryHeap::new(); //intialize the queue
+    
 
-    // j and i got flipped from standard notation i nested in j instead of the the other way around
-    for _i in 0..10 {
-        for j in 0..data.len() {
-            let mut exc = Vec::new(); //each bif loop has a new exculsion list as the heap is emptied each j iteration
-            exc.push(data[j][0].id);
-            heap.push(data[j][0]);
-            for i in 0..data[j].len() {
-                // if the id of the item to be added is not already in the exculsion list 
-                // it is added to the heap then it's id is added to the exclusion list
-                if  !exc.contains(&data[j][i].id)  { 
+    let mut heapt: BinaryHeap<Keyval> = BinaryHeap::new();
 
-                    exc.push(data[j][i].id);
-                    heap.push(data[j][i]);
-                } else if !heap.is_empty()  {
-                    while  !heap.is_empty() && heap.peek().unwrap().id != data[j][i].id {
-                        let y = heap.pop().unwrap().id;
-                        exc.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
-                    }
-                }
-                
-            }
-            //clears the queue after everything in a big timestep has been processed 
-            while !heap.is_empty() { 
-                heap.pop();
-                //exc.pop();
-                
+    for i in 0..data.len() {
+        //find a set of the first occuring unique index events
+        let mut unq_set: Vec<Keyval> = data[i].clone();
+        //sort them by index then order the indexs by time
+        unq_set.sort_by(|a,b| (a.id,a.key).partial_cmp(&(b.id,b.key)).unwrap());
+        unq_set.dedup_by(|a,b| a.id.eq(&b.id));
+        //add the first unique ids to the priority queue
+
+        for k in unq_set {
+            heapt.push(k);
+        }
+        while !heapt.is_empty() {
+            //pop the first element
+            let elem = heapt.pop().unwrap();
+            let id = elem.id;
+            let index = elem.index;
+            //if the set contains another element with the same id push the first occuring element into the priority queue
+            for k in &data[i][index+1..] {
+                if k.id == id {
+                    heapt.push(data[i][k.index]);  
+                } 
             }
         }
     }
@@ -110,32 +109,27 @@ fn main() {
 
     let mut heap1: bucketqueue::Bqueue<Keyval> = bucketqueue::Bqueue::new(((max/delta).ceil()+1.0) as usize,delta); //intialize the Bucket queue
 
-    // j and i got flipped 
-    for _i in 0..10 {
-        for j in 0..data.len() {
-            let mut exc1 = Vec::new(); //each j loop has a new exculsion list as the heap is emptied each j iteration
-            exc1.push(data[j][0].id);
-            heap1.push(data[j][0],data[j][0].val.time);
-            for i in 0..data[j].len() {
-                // if the id of the item to be added is not already in the exculsion list 
-                // it is added to the heap then it's id is added to the exclusion list
-                if  !exc1.contains(&data[j][i].id)  { 
+    for i in 0..data.len() {
+        //find a set of the first occuring unique index events
+        let mut unq_set: Vec<Keyval> = data[i].clone();
+        //sort them by index then order the indexs by time
+        unq_set.sort_by(|a,b| (a.id,a.key).partial_cmp(&(b.id,b.key)).unwrap());
+        unq_set.dedup_by(|a,b| a.id.eq(&b.id));
+        //add the first unique ids to the priority queue
 
-                    exc1.push(data[j][i].id);
-                    heap1.push(data[j][i], data[j][i].val.time);
-                } else if !heap1.is_empty()  {
-                    while  !heap1.is_empty() && heap1.peek().unwrap().id != data[j][i].id {
-                        let y = heap1.pop().unwrap().id;
-                        exc1.retain(|x| x != &y ); //still has to remove the ids that are popped from the heap before the repeat id
-                    }
-                }
-                
-            }
-            //clears the queue after everything in a big timestep has been processed 
-            while !heap1.is_empty() { 
-                heap1.pop();
-                //exc.pop();
-                
+        for k in unq_set {
+            heap1.push(k,*k.key);
+        }
+        while !heap1.is_empty() {
+            //pop the first element
+            let elem = heap1.pop().unwrap();
+            let id = elem.id;
+            let index = elem.index;
+            //if the set contains another element with the same id push the first occuring element into the priority queue
+            for k in &data[i][index+1..] {
+                if k.id == id {
+                    heap1.push(data[i][k.index], *k.key);  
+                } 
             }
         }
     }
